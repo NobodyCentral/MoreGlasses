5d94ed51d1d996ea5e90a30d118f340304e29c5b
NobodyCentral <milkingcow101@gmail.com>
2022-04-17 12:27:00 -0700

    Gave all blocks item models

--- src/main/java/com/nobody/moreglasses/blocks/GlassBase.java
+++ src/main/java/com/nobody/moreglasses/blocks/GlassBase.java
@@ -1,17 +1,15 @@
 package com.nobody.moreglasses.blocks;
 
-import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Block;
-import net.minecraft.block.GlassBlock;
-import net.minecraft.block.SoundType;
+import net.minecraft.block.*;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.material.MaterialColor;
 import net.minecraftforge.common.ToolType;
 
 public class GlassBase extends GlassBlock {
     public GlassBase() {
-        super(AbstractBlock.Properties.of(Material.GLASS)
-                .strength(0.3f, 0.3f)
-                .sound(SoundType.GLASS).noOcclusion());
+        super(AbstractBlock.Properties.copy(Blocks.GLASS).noOcclusion());
+        //                              of((Material.GLASS)
+        //                .strength(0.3f, 0.3f)
+        //                .sound(SoundType.GLASS).noOcclusion()));
     }
 }
